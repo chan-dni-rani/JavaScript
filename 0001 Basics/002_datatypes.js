bda1fc49756f4ede48d7d3e17ade515eeb7efd7a
chandni <ranichandnirani22827@gmail.com>
2026-03-19 12:17:17 +0000

    datatypes

---
+++ 0001 Basics/002_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+let name = "Chandni"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint -> 
+// string -> " "
+// boolean -> true/false
+// null -> standalon value
+// undefined -> 
+// symbol -> uniqu
+
+// object
+
+console.log(typeof age);
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
