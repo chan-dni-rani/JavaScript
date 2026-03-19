109dd3d631c1913f00bb3514384474b92be255bb
chandni <ranichandnirani22827@gmail.com>
2026-03-19 11:55:18 +0000

    variables

---
+++ 0001 Basics/001_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1234
+let accountEmail = "absc@gmail.com"
+var accountPassword = "12345"
+accountCity = "Punjab"
+
+// accountId = 2
+accountEmail = "ab@gmail.com"
+accountPassword = "68696"
+accountCity = "Jaipur"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
