47c93db2657023895074e2ad8b390c9310cc046e
chandni <ranichandnirani22827@gmail.com>
2026-03-20 12:16:55 +0000

    conversion

---
+++ 0001 Basics/003_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "33adg"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" -> 33
+// "33as" -> NaN
+// true -> 1; false-> 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
